edbb622a30bc47121346f11d279bd20287eafd21
Krowork <thelake.krowork@gmail.com>
2024-09-20 18:30:33 +0200

    Segundo cambio

--- holamundo.java
+++ holamundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
   public static void main (String[] args) {
     System.out.println("Hola, mundo!");
     System.out.println("Desde Ubuntu");
+    System.out.println("ESTO FUNCIONA");
   }
 }
